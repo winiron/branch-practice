b812e0cae1134bef2fef1b02729fe5483750325a
winiron <yamg1015@gmail.com>
2022-05-01 23:26:24 +0900

    feat : Print world if i is times of 5

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
-for i in range(1,10+1);
-    if i%3==0;
-        print('hello');
+for i in range(1,10+1):
+    if i%3==0:
+        print('{} hello'.forma(i))
+    else if i%==0:
+        print('{} world'.format(i))
